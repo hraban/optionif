234f50067a6dbe5e3dca1a4d6b98c6f6ad3ca8a4
Hraban Luyat <hraban@0brg.net>
2015-07-26 13:33:02 +0100

    Adapt standard test project to test Option.map

--- testproject/src/main/scala/net/obrg/optionif/Test.scala
+++ testproject/src/main/scala/net/obrg/optionif/Test.scala
@@ -4,9 +4,15 @@ package net.obrg.optionif
  * Simple test program that should trigger the DivByZero plugin, if loaded
  */
 object Test {
-  val five = 5
-  val amount = five / 0
   def main(args: Array[String]) {
-    println(amount)
+    // Not an Option.map
+    val x = List(1, 2, 3, 4).map(_ * 2)
+    // Option.map
+    val y = Some(4).map(_ * 2)
+    // Inferred Option.map
+    val z = List(Some(1), None, None, Some(4)).map(_.map(_ * 2))
+    // Transitive inferred Option map
+    val zsum = z.flatMap(x => x).sum
+    println(x.sum + y.sum + zsum)
   }
 }
