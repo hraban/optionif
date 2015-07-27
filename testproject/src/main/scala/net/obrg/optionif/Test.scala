ff9349db2398f39478b2919c74ae74b3fd65dd5d
Hraban Luyat <hraban@0brg.net>
2015-07-27 11:06:38 +0100

    Additional tests in test project

--- testproject/src/main/scala/net/obrg/optionif/Test.scala
+++ testproject/src/main/scala/net/obrg/optionif/Test.scala
@@ -9,10 +9,21 @@ object Test {
     val x = List(1, 2, 3, 4).map(_ * 2)
     // Option.map
     val y = Some(4).map(_ * 2)
+    val o: Option[Int] = y
+    val o2 = y.map(_ * 3)
+    val asdf = None.map(x => "nothing")
+    val asdf2 = None.map(x => 3)
+    val asdf3: Option[Int] = None
+    asdf3.map(x => 3)
+    val asdf4: Option[_] = None
+    asdf4.map(x => 3)
+    val noneval = None
+    noneval.map(x => "noneval")
+    val foo = if (o.isEmpty) None else Some(o.get * 3)
     // Inferred Option.map
     val z = List(Some(1), None, None, Some(4)).map(_.map(_ * 2))
     // Transitive inferred Option map
-    val zsum = z.flatMap(x => x).sum
+    val zsum = z.flatMap(_.map(_ * -1)).sum
     println(x.sum + y.sum + zsum)
   }
 }
